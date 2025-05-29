85e8929a7285c979feecc090e3de54aecb5f116b
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-05-29 12:26:20 +0700

    update2

--- lib/audio_helpers/player_invoke.dart
+++ lib/audio_helpers/player_invoke.dart
@@ -1,4 +1,5 @@
 // file: lib/audio_helpers/player_invoke.dart
+
 import 'dart:async';
 import 'package:flutter/foundation.dart'; // for kIsWeb
 import 'package:audio_service/audio_service.dart';
@@ -7,11 +8,6 @@ import 'package:music_player/audio_helpers/page_manager.dart';
 import 'package:music_player/audio_helpers/service_locator.dart';
 
 DateTime playerTapTime = DateTime.now();
-
-bool get isProcessForPlay {
-  return DateTime.now().difference(playerTapTime).inMilliseconds > 600;
-}
-
 Timer? debounce;
 
 /// Debounce để tránh bấm play quá nhanh
@@ -37,48 +33,42 @@ class PlayerInvoke {
     final List finalList = songsList.toList();
     if (shuffle) finalList.shuffle();
 
-    if (!fromMiniPlayer) {
-      if (!kIsWeb) {
-        // Chỉ stop trên mobile/desktop
-        await pageManager.stop();
-      }
-      await setValues(finalList, globalIndex);
-    } else {
-      await setValues(finalList, globalIndex);
+    if (!fromMiniPlayer && !kIsWeb) {
+      // Non-web: stop queue trước khi set mới
+      await pageManager.stop();
     }
+    await setValues(finalList, globalIndex);
   }
 
   /// Thiết lập queue và phát bài
-  static Future<void> setValues(
-      List arr,
-      int index, {
-        bool recommend = false,
-      }) async {
+  static Future<void> setValues(List arr, int index, {bool recommend = false}) async {
     final List<MediaItem> queue = arr
         .map((song) => MediaItemConverter.mapToMediaItem(song as Map, autoplay: recommend))
         .toList();
     await updateNPlay(queue, index);
   }
 
-  /// Cập nhật queue và gọi playAS
+  /// Cập nhật queue và gọi play
   static Future<void> updateNPlay(List<MediaItem> queue, int index) async {
     try {
+      // Tắt shuffle mode
       await pageManager.setShuffleMode(AudioServiceShuffleMode.none);
+      // Set lại toàn bộ queue, bắt đầu từ index
       await pageManager.adds(queue, index);
 
       final mediaItem = queue[index];
-      // Phân biệt play Web vs Mobile/Desktop
+      // Play tuỳ nền tảng
       if (kIsWeb) {
         await pageManager.playAS(mediaItem);
       } else {
         pageManager.play();
       }
 
+      // Cập nhật thời điểm tap để debounce lần kế
       playerTapTime = DateTime.now();
     } catch (e, stack) {
       debugPrint('⚠️ Error playing audio: $e');
-      debugPrint('$stack');
+      debugPrint(stack.toString());
     }
   }
-
 }
